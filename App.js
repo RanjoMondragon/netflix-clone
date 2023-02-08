f80c0cff2f74881a8cc35e8f147fcff6d214fe10
Ranjo Mondragon <115973531+RanjoMondragon@users.noreply.github.com>
2023-02-07 23:46:07 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Row title="NETFLIX ORIGINALS" />
+    <Row title="Trending Now" />
+      
+    </div>
+  );
+}
+
+export default App;
